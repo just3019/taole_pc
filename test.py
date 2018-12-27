2df51aa770d506e3b699c21c6226fc6405b94e99
songlinhao <455674848@qq.com>
2018-12-27 18:51:29 +0800

    update

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-from kl import kl_search_list
+import jieba as jieba
 
-if __name__ == '__main__':
-    # sn.sn_search_list("iphonexsmax")
-    kl_search_list("iphone")
+text = "Apple 苹果 iphone Xs Max 移动联通电信4G手机 深空灰 256GB 【全新原封 国行正品 全国联保 京东物流 快捷安全 抢购苹果XS 送无线充+游戏辅助按键+壳+膜+指环扣】抢购苹果XR"
+
+seg_list = jieba.cut(text, cut_all=True)
+print(",".join(seg_list))  # 全模式
+
+seg_list = jieba.cut(text, cut_all=False)
+print(",".join(seg_list))  # 精确模式
+
+seg_list = jieba.cut(text)  # 默认是精确模式
+print(",".join(seg_list))
+
+seg_list = jieba.cut_for_search(text)
+print(",".join(seg_list))
